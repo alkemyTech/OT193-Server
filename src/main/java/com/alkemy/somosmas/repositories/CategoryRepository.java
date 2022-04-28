4cdfae3cdced61e16b7cec037fd5835f8e4bf27f
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-04-28 19:41:30 -0300

    se crea entidad categoria y su repositorio

--- src/main/java/com/alkemy/somosmas/repositories/CategoryRepository.java
+++ src/main/java/com/alkemy/somosmas/repositories/CategoryRepository.java
@@ -1,5 +1,12 @@
 package com.alkemy.somosmas.repositories;
 
-public class CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alkemy.somosmas.models.Category;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category , Long>{
 
 }
