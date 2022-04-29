ea7368036e126c62aa444a7615bb2af6d0a03670
Juan Cerezole <jpc010499@gmail.com>
2022-04-28 23:55:04 -0300

    Entity testimonials added + repository + service

---
+++ src/main/java/com/alkemy/somosmas/models/Testimonial.java
@@ -0,0 +1,41 @@
+package com.alkemy.somosmas.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.Id;
+
+import com.sun.istack.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+@SQLDelete(sql="UPDATE testimonials SET deleted= true WHERE id=?")
+@Where(clause="deleted=false")
+@Table(name = "testimonials")
+public class Testimonial {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private Long id;
+	
+	@NotNull
+	private String name;
+	
+	private String image;
+	private String content;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createAt;
+	
+	private boolean deleted = Boolean.FALSE;
+}
---
+++ src/main/java/com/alkemy/somosmas/services/TestimonialServiceImpl.java
@@ -0,0 +1,5 @@
+package com.alkemy.somosmas.services;
+
+public class TestimonialServiceImpl implements TestimonialService {
+
+}
