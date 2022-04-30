98745f44f2b89b40d494f362cee16b81ea3de8d6
MarianoT <marianotoppino@gmail.com>
2022-04-30 12:59:28 -0300

    annotation Repository added

--- src/main/java/com/alkemy/somosmas/repositories/OrganizationRepository.java
+++ src/main/java/com/alkemy/somosmas/repositories/OrganizationRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.somosmas.repositories;
 
 import com.alkemy.somosmas.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization , Long> {
 }
