b145eb5be7810b985f3870d652b690402dd898af
fernandof <fittipaldifernando@gmail.com>
2022-04-30 20:09:32 -0300

    update generation type ID and time stamp

--- src/main/java/com/alkemy/somosmas/models/News.java
+++ src/main/java/com/alkemy/somosmas/models/News.java
@@ -1,6 +1,6 @@
 package com.alkemy.somosmas.models;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -29,32 +30,37 @@ import lombok.Setter;
 @SQLDelete(sql = "UPDATE news SET deleted = true WHERE id=?")
 @Where(clause = "deleted=false")
 public class News {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(unique = true, nullable = false)
-	private Long id; 
-	
+	private Long id;
+
 	@Column(nullable = false)
-	private String name; 
-	
+	private String name;
+
 	@Column(nullable = false)
-	private String content; 
-	
+	private String content;
+
 	@Column(nullable = false)
-	private String image; 
-	
+	private String image;
+
 	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "category_id", insertable = false, updatable = false)
-	private Category category; 
-	
+	private Category category;
+
 	@Column(name = "category_id", nullable = false)
-	private Long categoryId; 
-	
+	private Long categoryId;
+
 	private boolean deleted = Boolean.FALSE;
-	
-	@Column(name = "create_at")
-	@Temporal(TemporalType.TIMESTAMP)
+
+	@Column(name = "create_date")
 	@DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
-	private LocalDate createAt;
+	private LocalDateTime createDate;
+
+	@PrePersist
+	private void beforePersisting() {
+		this.createDate= LocalDateTime.now();
+	}
+
 }
