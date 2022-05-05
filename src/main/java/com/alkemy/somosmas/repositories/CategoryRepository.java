955168729a4f6528b63fbd55e082d573cd946e4b
duccisg <sebastianducci@gmail.com>
2022-05-05 16:27:32 -0300

    Revert "Ot193 17"

--- src/main/java/com/alkemy/somosmas/repositories/CategoryRepository.java
+++ src/main/java/com/alkemy/somosmas/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.alkemy.somosmas.repositories;
 
-import com.alkemy.somosmas.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.alkemy.somosmas.models.Category;
+
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface CategoryRepository extends JpaRepository<Category , Long>{
+
 }
