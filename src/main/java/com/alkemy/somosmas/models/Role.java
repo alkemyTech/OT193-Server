2e6b76ebb3ec9ee8f4323a24f0c9df77c5d5bccd
Fabio Olivera <fabio.olivera@i2t-sa.com.ar>
2022-05-05 17:03:11 -0300

    Cambios en Role, OngApplication y application.properties

--- src/main/java/com/alkemy/somosmas/models/Role.java
+++ src/main/java/com/alkemy/somosmas/models/Role.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name= "roles")
+@Table(name= "role")
 @Data
 public class Role {
 
--- src/main/java/com/alkemy/somosmas/ong/OngApplication.java
+++ src/main/java/com/alkemy/somosmas/ong/OngApplication.java
@@ -1,4 +1,4 @@
-package com.alkemy.somosmas;
+package com.alkemy.somosmas.ong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
