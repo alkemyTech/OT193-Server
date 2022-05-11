fb51a36e736936e2b3919751be83d11dd7b72165
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-05-11 13:03:28 -0300

    Se agregan clases RoleService y RoleSErviceImpl, y se arregla UserDTO

--- src/main/java/com/alkemy/somosmas/mappers/UserMapper.java
+++ src/main/java/com/alkemy/somosmas/mappers/UserMapper.java
@@ -2,22 +2,33 @@ package com.alkemy.somosmas.mappers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alkemy.somosmas.dtos.UserDTO;
+import com.alkemy.somosmas.models.Role;
 import com.alkemy.somosmas.models.User;
+import com.alkemy.somosmas.repositories.RoleRepository;
 
 @Component
 public class UserMapper {
 
+	@Autowired
+	RoleRepository roleRepository;
+
 	public User userDTO2Entity(UserDTO userDTO) {
+		Optional<Role> role = roleRepository.findById(userDTO.getRole());
 		User user = new User();
 		user.setEmail(userDTO.getEmail());
 		user.setPassword(userDTO.getPassword());
 		user.setFirstName(userDTO.getFirstName());
 		user.setLastName(userDTO.getLastName());
 		user.setPhoto(userDTO.getPhoto());
+		if(role.isPresent()) {
+			user.setRole(role.get());
+		}
 		return user;
 	}
 
---
+++ src/main/java/com/alkemy/somosmas/services/RoleService.java
@@ -0,0 +1,13 @@
+package com.alkemy.somosmas.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.alkemy.somosmas.models.Role;
+
+
+@Service
+public interface RoleService {
+	Optional<Role> getById(Long id);
+}
