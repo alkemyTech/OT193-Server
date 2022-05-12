fb91068f3ede0934d8ba1d1658385930724c7469
Meistt <aguslive7@gmail.com>
2022-05-12 01:06:25 -0300

    Endpoint listar usuarios con DTO listo

--- src/main/java/com/alkemy/somosmas/OngApplication.java
+++ src/main/java/com/alkemy/somosmas/OngApplication.java
@@ -2,6 +2,7 @@ package com.alkemy.somosmas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class OngApplication {
--- src/main/java/com/alkemy/somosmas/services/UserServiceImpl.java
+++ src/main/java/com/alkemy/somosmas/services/UserServiceImpl.java
@@ -1,15 +1,34 @@
 package com.alkemy.somosmas.services;
 
+import com.alkemy.somosmas.dtos.UserDTO;
+import com.alkemy.somosmas.mappers.UserMapper;
+import com.alkemy.somosmas.models.User;
+import com.alkemy.somosmas.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService ,UserDetailsService {
 
-	
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private UserMapper userMapper;
+
+	/*@Autowired
+	public UserServiceImpl(UserRepository userRepository, UserMapper userMapper){
+		this.userRepository = userRepository;
+		this.userMapper = userMapper;
+	}*/
+
+
 	@Override
 	@Transactional(readOnly=true)
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -17,4 +36,31 @@ public class UserServiceImpl implements UserService ,UserDetailsService {
 		return null;
 	}
 
+	@Override
+	public Boolean exists(Long id) {
+		Boolean exists = this.userRepository.existsById(id);
+		if(exists){
+			return exists;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public User getUser(Long id) {
+		if(exists(id)){
+			User user = new User();
+			user = this.userRepository.getById(id);
+			return user;
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public List<UserDTO> getUsersList() {
+		List<User> model = userRepository.findAll();
+		List<UserDTO> dtoList = userMapper.modelUserToUserDTO(model);
+		return dtoList;
+	}
 }
