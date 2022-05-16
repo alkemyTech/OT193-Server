ee96af6e7c214166abca948201324fe980622831
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-05-16 13:37:28 -0300

    manejo de roles

--- src/main/java/com/alkemy/somosmas/auth/SpringSecurityConfig.java
+++ src/main/java/com/alkemy/somosmas/auth/SpringSecurityConfig.java
@@ -45,8 +45,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//	        http.authorizeRequests()
-//	                .antMatchers("/user/test").hasAuthority("ADMIN");
+	        http.authorizeRequests()
+	        	.antMatchers("/**").hasAuthority("ADMIN")
+	        	.antMatchers("/public/*").hasAuthority("USER");
+
 	}
 
 }
