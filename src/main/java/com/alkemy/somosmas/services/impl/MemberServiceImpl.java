d3e0fc3599b9a90aeb7e822ead5d4849b626b702
fernandof <fittipaldifernando@gmail.com>
2022-05-16 23:49:52 -0300

    upload deleteMember

--- src/main/java/com/alkemy/somosmas/services/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/somosmas/services/impl/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alkemy.somosmas.services.impl;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,14 @@ public class MemberServiceImpl implements MemberService {
 		MemberDTO result = this.memberMapper.memberEntity2DTO(memberEntitySaved);
 		return result;
 
+	}
+
+	@Override
+	public void delete(Long id) {
+		if (!this.memberRepository.existsById(id)) {
+			//throw  
+		}
+		this.memberRepository.deleteById(id);
 	}
 
 }
