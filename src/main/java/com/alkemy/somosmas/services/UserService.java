2017dcf240211d693e8464c4048103eaa0db10d0
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-05-19 14:22:55 -0300

    prueba

--- src/main/java/com/alkemy/somosmas/services/UserService.java
+++ src/main/java/com/alkemy/somosmas/services/UserService.java
@@ -1,13 +1,21 @@
 package com.alkemy.somosmas.services;
 
-import com.alkemy.somosmas.dtos.UserDTO;
-import com.alkemy.somosmas.models.User;
-
 import java.util.List;
 
-public interface UserService  {
+import org.springframework.stereotype.Service;
+
+import com.alkemy.somosmas.dtos.UserDTO;
+import com.alkemy.somosmas.exceptions.InvalidUserException;
+import com.alkemy.somosmas.models.User;
+
+@Service
+public interface UserService{
     UserDTO getUser(Long id);
     List<UserDTO> getUsersList();
     UserDTO registerUserDTO2Model(UserDTO userDTO) throws Exception;
     Boolean deleteUser(Long id);
+    User findByEmail(String email);
+	Boolean existsByEmail(String email);
+	UserDTO authUser(String email, String password) throws InvalidUserException;
+	UserDTO user2UserDTO(User user);
 }
--- src/main/java/com/alkemy/somosmas/services/UserServiceImpl.java
+++ src/main/java/com/alkemy/somosmas/services/UserServiceImpl.java
@@ -4,15 +4,22 @@ import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.alkemy.somosmas.dtos.UserDTO;
+import com.alkemy.somosmas.exceptions.InvalidUserException;
 import com.alkemy.somosmas.mappers.UserMapper;
 import com.alkemy.somosmas.models.User;
 import com.alkemy.somosmas.repositories.UserRepository;
@@ -21,9 +28,16 @@ import com.alkemy.somosmas.repositories.UserRepository;
 public class UserServiceImpl implements UserService ,UserDetailsService {
 
 	@Autowired
-	private UserRepository userRepository;
+	UserRepository userRepository;
+	@Autowired
+	UserDetailsService userDetailsService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private UserMapper userMapper;
+	@Autowired
+	@Qualifier("authenticationManager")
+	private AuthenticationManager authenticationManager;
 
 
 	@Override
@@ -83,7 +97,39 @@ public class UserServiceImpl implements UserService ,UserDetailsService {
 		}
 	}
 
+	private void injectUserInSecurityContext(String email, String password) {
+		Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email,password));
+		SecurityContextHolder.getContext().setAuthentication(auth);
+	}
+
+	@Override
+	public UserDTO authUser(String email, String password) throws InvalidUserException {
+		if(!existsByEmail(email)) {
+			throw new InvalidUserException("Email does not exist");
+		}
+		User user = findByEmail(email);
+		if(!passwordEncoder.matches(password,user.getPassword())) {
+			throw new InvalidUserException("Incorrect email or password");
+		}
+		UserDTO userDTO = user2UserDTO(user);
+		injectUserInSecurityContext(email, password);
+		return userDTO;
+	}
 
 
+	@Override
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@Override
+	public Boolean existsByEmail(String email) {
+		return userRepository.existsByEmail(email);
+	}
+	@Override
+	public UserDTO user2UserDTO(User user) {
+		UserDTO userDTO = userMapper.originalToDTO(user);
+		return userDTO;
+	}
 
 }
