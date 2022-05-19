71ae156abd7aa481f6ee86043e72a5d5ac216a48
MateoOllocco <mateoollocco@live.com>
2022-05-19 16:40:57 -0300

    Se implemento los metodos save y update en activityService

--- src/main/java/com/alkemy/somosmas/services/impl/ActivityServiceImpl.java
+++ src/main/java/com/alkemy/somosmas/services/impl/ActivityServiceImpl.java
@@ -3,6 +3,7 @@ package com.alkemy.somosmas.services.impl;
 import com.alkemy.somosmas.dtos.ActivityDTO;
 import com.alkemy.somosmas.mappers.ActivityMapper;
 import com.alkemy.somosmas.models.Activity;
+import com.alkemy.somosmas.models.Organization;
 import com.alkemy.somosmas.repositories.ActivityRepository;
 import com.alkemy.somosmas.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,36 @@ public class ActivityServiceImpl implements ActivityService {
     @Autowired
     private ActivityMapper activityMapper;
 
-    public ActivityDTO save(ActivityDTO dto){
-        Activity activity = activityMapper.activityDTO2Model(dto);
-        Activity activitySave = activityRepository.save(activity);
-        return activityMapper.activityModel2DTO(activitySave);
+    @Override
+    public ActivityDTO save(ActivityDTO dto) throws ModelNotFoundException {
+        Activity model=null;
+        // Si tiene un id lo busco en la base de datos sino lo encuentro lanzo una excepcion.
+        if(dto.getId()!=null && dto.getId()!=0){
+            model = this.activityRepository.findById(dto.getId()).orElse(null);
+            // Esta excepcion arreglarse si eventualmente se usa el metodo.
+            if(model==null){
+                //Excepcion de tipo check heredar de la clase exception
+                throw new ModelNotFoundException(dto.getId(),"Organization");
+            }
+        }
+        model = this.activityMapper.activityDTO2Model(dto);
+        Activity modelSaved = activityRepository.save(model);
+        ActivityDTO result = activityMapper.activityModel2DTO(modelSaved);
+
+        return result;
+    }
+
+    @Override
+    public ActivityDTO update(Long id, ActivityDTO dto) throws ModelNotFoundException {
+        Activity model = this.activityRepository.findById(id).orElse(null);
+        if(model==null){
+            //Excepcion de tipo check heredar de la clase exception
+            throw new ModelNotFoundException(id,"Activity");
+        }
+        model= this.activityMapper.organizationRefreshValues(model, dto);
+        Activity modelSaved = activityRepository.save(model);
+        ActivityDTO result = activityMapper.activityModel2DTO(modelSaved);
+
+        return result;
     }
 }
