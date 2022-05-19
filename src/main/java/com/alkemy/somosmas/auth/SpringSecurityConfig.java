13bf4e4dd6b526ebf8d8e37271e9f420113a6240
Yesica Oviedo <oviedoyesica52@gmail.com>
2022-05-19 16:31:00 -0300

    dejo todo como para replicar error

--- src/main/java/com/alkemy/somosmas/auth/SpringSecurityConfig.java
+++ src/main/java/com/alkemy/somosmas/auth/SpringSecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,24 +18,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private UserDetailsService userService;
-	
+
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	/* Se agrego durante el meet */
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**");
-	}
-
-	/* Para validar que el usuario sea de rol ADMIN*/
-	@Override
-	public void configure(HttpSecurity httpSecurity) throws Exception {
-	httpSecurity.csrf().disable();
-		httpSecurity.authorizeRequests().antMatchers("/categories/**").hasRole("ADMIN")
-				.and()
-				.authorizeRequests().anyRequest().permitAll();}
+//	/* Se agrego durante el meet */
+//	@Override
+//	public void configure(WebSecurity web) throws Exception {
+//		web.ignoring().antMatchers("/**");
+//	}
+//
+//	/* Para validar que el usuario sea de rol ADMIN*/
+//	@Override
+//	public void configure(HttpSecurity httpSecurity) throws Exception {
+//	httpSecurity.csrf().disable();
+//		httpSecurity.authorizeRequests().antMatchers("/categories/**").hasRole("ADMIN")
+//				.and()
+//				.authorizeRequests().anyRequest().permitAll();}
 
 	@Override
 	@Autowired
@@ -47,5 +47,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers(new String[]{"/auth/register", "/auth/login", "/h2/**"}).permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/test").hasAnyAuthority("ADMIN");
 	}
 }
