2e426e772dd3b5764d278a326daf957cd730067f
cristhian <guc4530@gmail.com>
2022-05-26 08:12:06 -0500

    adicion de deletefile para update  image

--- src/main/java/com/alkemy/somosmas/dtos/SlideGetDTO.java
+++ src/main/java/com/alkemy/somosmas/dtos/SlideGetDTO.java
@@ -15,29 +15,6 @@ public class SlideGetDTO {
     public SlideGetDTO() {
     }
 
-   /* public String getImageUrl() {
-        return imageUrl;
-    }
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    public Integer getOrder() {
-        return order;
-    }
-    public void setOrder(Integer order) {
-        this.order = order;
-    }
-    public String getText() {
-        return text;
-    }
-    public void setText(String text) {
-        this.text = text;
-    }
-    public Long getOrganizationId() {
-        return organizationId;
-    }
-    public void setOrganizationId(Long organizationId) {
-        this.organizationId = organizationId;
-    }*/
+   
 
 }
