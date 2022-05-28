42c781ecce09003da7f1413c002b29ce5dd309b5
MarianoT <marianotoppino@gmail.com>
2022-05-28 14:29:09 -0300

    method to do pagination in NewsService added

--- src/main/java/com/alkemy/somosmas/services/NewsService.java
+++ src/main/java/com/alkemy/somosmas/services/NewsService.java
@@ -2,6 +2,10 @@ package com.alkemy.somosmas.services;
 
 import com.alkemy.somosmas.dtos.NewsDTO;
 import com.alkemy.somosmas.exceptions.ModelNotFoundException;
+import com.alkemy.somosmas.exceptions.NotAcceptableArgumentException;
+import com.alkemy.somosmas.exceptions.PageEmptyException;
+
+import java.util.Map;
 
 public interface NewsService {
 
@@ -12,5 +16,7 @@ public interface NewsService {
 	NewsDTO getDetailsById(Long id) throws ModelNotFoundException;
 
 	NewsDTO update(Long id, NewsDTO dto) throws ModelNotFoundException;
+
+	Map<String, Object> getAllNewsByPage(int pageNo ) throws NotAcceptableArgumentException, PageEmptyException;
 
 }
